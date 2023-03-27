cd66e762c402a171b59db328378f31a00134ae17
Ian Jarvis <ijarvis12@gmail.com>
2023-03-27 05:45:44 -0500

    initial commit

---
+++ fib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function fib(){
+    if [[ $1 -eq 1 ]]; then
+        echo $(($2+$3))
+    else
+        let "z = $2 + $3"
+        echo $z
+        fib $(($1-1)) $3 $z
+    fi
+}
+
+echo "Calculate the Fibonacci Sequence"
+echo "Enter Fib Number to Go Until:"
+declare -i n
+read n
+echo "The Sequence:"
+echo "1"
+echo "1"
+fib $n 1 1
+exit 0
